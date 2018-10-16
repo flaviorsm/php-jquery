d0a0e21d0351e041d77bf8cda6c17ec129ec1d06
Flavio Moreira <flaviorsm@gmail.com>
2018-10-15 22:12:52 -0300

    Ajustes finais

--- app/contact.php
+++ app/contact.php
@@ -11,12 +11,10 @@ $message = '<html><head><title>' . $subject . '</title></head>'
         . '<tr><td>Nome : </td><td> ' . $userName . '</td></tr>'
         . '<tr><td>Mensagem : </td><td> ' . $userMsg . '</td></tr>'
         . '</table></body></html>';
-//$message = "Email id :  ".$userEmail. "\r\nPhone No : ".$userPhone."\r\nName : ".$userName."\r\nSays : ".$userMsg;
 
 $to = $email_id;
 $headers = "From: " . strip_tags($userEmail) . "\r\n";
 $headers .= "Reply-To: " . strip_tags($userEmail) . "\r\n";
-//$headers .= "CC: susan@example.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
